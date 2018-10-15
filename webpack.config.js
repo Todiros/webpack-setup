5516904d9a193baa9a596c47de075ff5df325c55
Tony Borisov <tony.d.borisov@gmail.com>
2018-10-15 20:27:27 +0300

    fix: remove unused packages/loaders

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             exclude: /node-modules/,
             use: { 
                 loader: 'babel-loader',
-                options: { presets: ['@babel/preset-env', '@babel/preset-react']} 
+                options: { presets: ['@babel/preset-env']} 
             }
         },
         {
